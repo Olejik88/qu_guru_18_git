6d38a3fcde66f88446760cd6889f76036e042bca
Olejik <beloff.o@yandex.ru>
2023-11-23 18:26:44 +0300

    delete trash

---
+++ src/test/java/examples/JUnite5Examples.java
@@ -0,0 +1,13 @@
+package examples;
+
+import org.junit.jupiter.api.Test;
+
+public class JUnite5Examples {
+
+    @Test
+    void firstTest() {
+        System.out.println("______firstTest");
+                    }
+
+
+}
